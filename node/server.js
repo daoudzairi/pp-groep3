e89fa9e988c3fb432ccc15284ab1d2477f36e527
TischaNy <tischa.nyanguile@student.ehb.be>
2019-05-10 13:54:41 +0200

    Added API calls

--- node/server.js
+++ node/server.js
@@ -1,11 +1,53 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+const connection = mysql.createConnection(require('./config.json'));
 
-    res.sendFile(path.join(__dirname + '/html/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/api/books', (req, res) => {
+    connection.query('SELECT * FROM `tblProduct`', (err, results, fields)  => {
+            if(err) throw err;
+            res.json(results);
+        }
+    );
+});
+
+app.get('/api/search/id=:id', (req, res) => {
+    connection.query(`SELECT * FROM tblProduct WHERE ProductID = ${req.params.id}`, (err, results, fields)  => {
+        if(err) throw err;
+        res.json(results);
+    });
+});
+
+app.get('/api/search/titel=:titel', (req, res) => {
+    connection.query(`SELECT * FROM tblProduct WHERE Titel LIKE '%${req.params.titel}%'`, (err, results, fields) => {
+        if(err) throw err;
+        res.json(results);
+    })
+});
+
+app.get('/api/search/uitvoerder=:uitvoerder', (req, res) => {
+    connection.query(`SELECT * FROM tblProduct WHERE Uitvoerder LIKE '%${req.params.uitvoerder}%'`, (err, results, fields) => {
+        if(err) throw err;
+        res.json(results);
+    })
+});
+
+app.get('/api/search/genre=:genre', (req, res) => {
+    connection.query(`SELECT * FROM tblProduct WHERE Genre LIKE '%${req.params.genre}%'`, (err, results, fields) => {
+        if(err) throw err;
+        res.json(results);
+    })
+});
+
+app.use((req, res) => {
+    res.status(404).json({msg: "Could not find specified file."});
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
