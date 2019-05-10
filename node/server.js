dc76ebd50da46d9c60f093f5f83e77967dd3ffda
Sacha.Smets <sacha.smets@student.ehb.be>
2019-05-10 09:24:17 +0200

    bringing origin Sacha up to date

--- node/server.js
+++ node/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+<<<<<<< Updated upstream
 const path = require('path');
 const app = express();
 const port = 3000;
@@ -7,5 +8,11 @@ app.get('/', (req, res) => {
 
     res.sendFile(path.join(__dirname + '/html/index.html'));
 });
+=======
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+>>>>>>> Stashed changes
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
